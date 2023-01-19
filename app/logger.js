88f1ea126bbba83514fc1f19395bf9738d5b2aa0
Jonathan Gray <jonmattgray@gmail.com>
2023-01-19 11:45:54 +0000

    tests passing

--- app/logger.js
+++ app/logger.js
@@ -1,14 +1,16 @@
-const pino = require('pino')
-const env = require('./env')
+import pino from 'pino'
+import env from './env.js'
+
+const { SERVICE_TYPE, LOG_LEVEL } = env
 
 const logger = pino(
   {
-    name: env.SERVICE_TYPE,
-    level: env.LOG_LEVEL,
+    name: SERVICE_TYPE,
+    level: LOG_LEVEL,
   },
   process.stdout
 )
 
 logger.debug('Environment variables: %j', { ...env })
 
-module.exports = logger
+export default logger
